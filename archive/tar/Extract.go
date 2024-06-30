cc24e633bbd03f2e59471c521c2e4a12ab6d0e5f
Chara-X <1719552189@qq.com>
2024-07-01 06:15:15 +0800

    Add os

--- archive/tar/Extract.go
+++ archive/tar/Extract.go
@@ -6,7 +6,7 @@ import (
 	"path"
 )
 
-func Decompress(from, to string) error {
+func Extract(from, to string) error {
 	os.MkdirAll(to, 0777)
 	var cmd = exec.Command("tar", "-xf", from, "-C", to)
 	if path.Ext(from) == ".gz" {
